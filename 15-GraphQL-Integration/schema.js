484ac1b16b2c1499f606de1d117418646a01d294
Autocommit <you@example.com>
2026-09-12 00:41:42 +0530

    feat(graphql): define GraphQL schema

---
+++ 15-GraphQL-Integration/schema.js
@@ -0,0 +1,30 @@
+const { gql } = require('graphql-tag');
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Book" type defines the queryable fields for every book in our data source.
+  type Book {
+    id: ID!
+    title: String
+    author: String
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "books" query returns an array of zero or more Books (defined above).
+  type Query {
+    books: [Book]
+    book(id: ID!): Book
+  }
+  
+  # The "Mutation" type lists all available data modifications
+  type Mutation {
+    addBook(title: String!, author: String!): Book
+  }
+`;
+
+module.exports = typeDefs;
